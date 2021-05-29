184b6761adf2e50e4b2a9a7ab0cfe6f022a90489
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-29 18:31:00 +0000

    Add droid data to seed droid table

---
+++ db/seeds/02_droids.sql
@@ -0,0 +1,9 @@
+INSERT INTO droids (sellers_id, name, description, price, manufacturer, model)
+VALUES
+(1, 'P2-series astromech droid', "The P2-series astromech droid was Industrial Automaton's first astromech droid prototype for the R-series, using a rounded body design that would later prove popular with the R2-series astromech droids.", 2500, 'Industrial Automaton', 'Astromech Droid'),
+
+(1, 'R1-series astromech droid', "The R1-series astromech droid, also known as the R1 reactor drone, was Industrial Automaton's first production model of their new consumer-market R-series astromech droid line.", 1500, 'Industrial Automaton', 'Astromech Droid'),
+
+(2, 'BB-8', "BB-8, sometimes spelled and pronounced Beebee-Ate and nicknamed BB, was a BB-series astromech droid who operated approximately thirty years after the Battle of Endor.", 20, 'Industrial Automaton', 'BB-series Astromech Droid'),
+
+(2, 'C-3PO protocol droid', "Fluent in over 6 million forms of communication, the 3PO droid excelled at human-cyborg relations. PO-series droid, was a model of protocol droid produced by Cybot Galactica sometime prior to the Invasion of Naboo.", 1150, 'Cybot Galactica', 'Protocol droid'),
