38e8b727cb2721b6e0d499794ee4f1633677a117
Matthew Thompson <supra__charged@hotmail.com>
2021-05-29 12:15:08 -0600

    Edit user schema and readme

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
   id SERIAL NOT NULL PRIMARY KEY,
   avatar_url VARCHAR(255),
   email VARCHAR(255) NOT NULL,
-  is_admin BOOLEAN NOT NULL,
+  is_admin BOOLEAN NOT NULL DEFAULT FALSE,
   mobile_phone VARCHAR(50) NOT NULL,
   name VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
