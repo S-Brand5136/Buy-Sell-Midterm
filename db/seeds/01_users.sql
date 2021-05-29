5abddfcf8a2b7313d4e28ad28fe7831f859745fd
Matthew Thompson <supra__charged@hotmail.com>
2021-05-29 13:14:59 -0600

    Move avatar images to user_avatars subfolder; update seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO users (avatar_url, email, is_admin, mobile_phone, name, password)
-VALUES ('images/Star-wars-clip-art-free-download-yoda.gif', 'yoda@jedi.force', TRUE, '555-555-5555', 'Yoda', 'Password!');
+VALUES ('images/user_avatars/Star-wars-clip-art-free-download-yoda.gif', 'yoda@jedi.force', TRUE, '555-555-5555', 'Yoda', 'Password!');
 
 INSERT INTO users (avatar_url, email, is_admin, mobile_phone, name, password)
-VALUES ('images/Star-wars-clip-art-images-R2D2.gif', 'r2d2@rebellion.org', TRUE, '555-555-2222', 'R2-D2', 'Password!');
+VALUES ('images/user_avatars/Star-wars-clip-art-images-R2D2.gif', 'r2d2@rebellion.org', TRUE, '555-555-2222', 'R2-D2', 'Password!');
