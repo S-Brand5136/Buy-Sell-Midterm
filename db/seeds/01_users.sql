54aad35c7eb0cf4efa24b8af553d31764e7858ae
Matthew Thompson <supra__charged@hotmail.com>
2021-05-29 05:10:58 +0000

    Update db seed files with two new user seeds with images in public folder

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (avatar_url, email, is_admin, mobile_phone, name, password)
+VALUES ('images/Star-wars-clip-art-free-download-yoda.gif', 'yoda@jedi.force', TRUE, '555-555-5555', 'Yoda', 'Password!');
+
+INSERT INTO users (avatar_url, email, is_admin, mobile_phone, name, password)
+VALUES ('images/Star-wars-clip-art-images-R2D2.gif', 'r2d2@rebellion.org', TRUE, '555-555-2222', 'R2-D2', 'Password!');
