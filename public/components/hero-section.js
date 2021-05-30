439b50633df6e7b26aef02bb9a60a2b1d258015c
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-30 00:54:49 +0000

    add hero-section component in /components

---
+++ public/components/hero-section.js
@@ -0,0 +1,17 @@
+$(() => {
+  const heroSection = `
+  <section id="hero-section">
+  <div>
+    <div class="hero-message">
+      <h2>#1 Market Place to Buy and Sell Droids</h2>
+      <span>Search our users extensive selection of reclaimed droids</span>
+    </div>
+    <form action="">
+      <input type="text" name="search_input" id="search-bar">
+      <input class="btn btn-outline-info input-btn" type="submit" value="Search">
+    </form>
+  </div>
+  <img src="./images/sandCrawler.png" class="img-fluid" alt="">
+</section>
+  `;
+});
