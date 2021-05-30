b0472f8a78ee85d0ebdad3375f648eca1a620bd2
Matthew Thompson <supra__charged@hotmail.com>
2021-05-29 22:36:17 -0600

    Refactor featuredDroids function to be more DRY

--- public/components/featured_droids.js
+++ public/components/featured_droids.js
@@ -1,62 +1,32 @@
-/*
-  Pass in an array of droid objects. Each droid object will be of the form:
-    {
-      id: Number (integer),
-      sellers_id: Number (integer),
-      name: string,
-      description: string,
-      price: Number (integer),
-      manufacturer: string,
-      model: string,
-      start_date: Date,
-      end_date: Date,
-      image_url: string
-    }
-*/
+// Pass in and object where the key droids is an array of featured droids.
 const featuredDroids = function({droids}) {
   for (let i = 0; i < droids.length; i++) {
+    let inner = '';
     if (i === 0) {
       $('#featuredIndicators').append(`<button type="button" data-bs-target="#featuredCarousel" data-bs-slide-to="${i}" class="active"></button>`)
-      $('#featuredDroids').append(`
-        <div class="carousel-item active">
-          <div>
-            <img src="${droids[i].image_url}" class="d-block" alt="A droid named ${droids[i].name}">
-            <div class="card text-light m-0 border-0">
-              <div class="card-body">
-                <h4 class="card-title"><span>${droids[i].droid_name}</span> - $${droids[i].price}</h4>
-                <p class="card-text">${droids[i].description}</p>
-                <h5>Details</h5>
-                <ul>
-                  <li>Manufacturer: ${droids[i].manufacturer}</li>
-                  <li>Model: ${droids[i].model}</li>
-                  <li>Seller: ${droids[i].seller}</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      `);
+      inner += '<div class="carousel-item active">';
     } else {
       $('#featuredIndicators').append(`<button type="button" data-bs-target="#featuredCarousel" data-bs-slide-to="${i}"></button>`);
-      $('#featuredDroids').append(
-        `<div class="carousel-item">
-          <div>
-            <img src="${droids[i].image_url}" class="d-block" alt="A droid named ${droids[i].name}">
-            <div class="card text-light m-0 border-0">
-              <div class="card-body">
-                <h4 class="card-title"><span>${droids[i].droid_name}</span> - $${droids[i].price}</h4>
-                <p class="card-text">${droids[i].description}</p>
-                <h5>Details</h5>
-                <ul>
-                  <li>Manufacturer: ${droids[i].manufacturer}</li>
-                  <li>Model: ${droids[i].model}</li>
-                  <li>Seller: ${droids[i].seller}</li>
-                </ul>
-              </div>
-            </div>
+      inner += '`<div class="carousel-item">';
+    }
+    inner += `
+      <div>
+        <img src="${droids[i].image_url}" class="d-block" alt="A droid named ${droids[i].name}">
+        <div class="card text-light m-0 border-0">
+          <div class="card-body">
+            <h4 class="card-title"><span>${droids[i].droid_name}</span> - $${droids[i].price}</h4>
+            <p class="card-text">${droids[i].description}</p>
+            <h5>Details</h5>
+            <ul>
+              <li>Manufacturer: ${droids[i].manufacturer}</li>
+              <li>Model: ${droids[i].model}</li>
+              <li>Seller: ${droids[i].seller}</li>
+            </ul>
           </div>
         </div>
-      `);
-    }
+      </div>
+    </div>
+    `;
+    $('#featuredDroids').append(inner);
   }
 };
