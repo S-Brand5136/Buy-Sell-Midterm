3385b78e8e7f5987c4d55f6ad25613b502feedb4
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-30 03:33:00 +0000

    Add route to GET user by id

--- routes/users.js
+++ routes/users.js
@@ -5,20 +5,31 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
-      .then(data => {
+      .then((data) => {
         const users = data.rows;
         res.json({ users });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    console.log(req.params);
+    const queryParams = [req.params.id];
+    db.query(`SELECT * FROM users WHERE id = $1;`, queryParams)
+      .then((data) => {
+        console.log(data);
+        return res.status(400).json({ user: data.rows });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
