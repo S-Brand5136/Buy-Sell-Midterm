5f6f5e3b87a98a8b3aef0b7a336b970972fc3ab0
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-30 17:38:25 +0000

    add user to seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,7 @@
 -- Users table seeds here (Example)
 INSERT INTO users (avatar_url, email, is_admin, mobile_phone, name, password)
-VALUES ('images/user_avatars/Star-wars-clip-art-free-download-yoda.gif', 'yoda@jedi.force', TRUE, '555-555-5555', 'Yoda', 'Password!');
+VALUES ('images/user_avatars/Star-wars-clip-art-free-download-yoda.gif', 'yoda@jedi.force', TRUE, '555-555-5555', 'Yoda', 'Password!'),
+('images/user_avatars/Star-wars-clip-art-images-R2D2.gif', 'r2d2@rebellion.org', TRUE, '555-555-2222', 'R2-D2', 'Password!');
 
-INSERT INTO users (avatar_url, email, is_admin, mobile_phone, name, password)
-VALUES ('images/user_avatars/Star-wars-clip-art-images-R2D2.gif', 'r2d2@rebellion.org', TRUE, '555-555-2222', 'R2-D2', 'Password!');
+INSERT INTO users (email, mobile_phone, name, password)
+VALUES ('anakin@jedi.force', '555-555-2222', 'Anakin Skywalker', 'Password!');
