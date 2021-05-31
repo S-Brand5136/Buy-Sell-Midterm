d72d810fc003c4c2ba831fc8819103308bbf10b4
Matthew Thompson <supra__charged@hotmail.com>
2021-05-31 13:52:25 -0600

    Serve index file using ejs to fix script loading bug

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ db.connect();
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
@@ -54,8 +55,8 @@ app.use("/api/images", imagesRoutes(db));
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-app.get("*", (req, res) => {
-  return res.sendFile(`${__dirname}/public/index.html`);
+app.get("/*", (req, res) => {
+  return res.render('index');
 });
 
 app.listen(PORT, () => {
