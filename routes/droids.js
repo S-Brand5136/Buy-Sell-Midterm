0009f36039275cec604e49c470aa391eb28d5f88
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-31 17:14:29 +0000

    Update get all droids query to filter sold droids, add comments for new routes

--- routes/droids.js
+++ routes/droids.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
+  // GET: all droids
+  // RETURN: json object
+  // ACCCESS: public
   router.get("/", (req, res) => {
-    const queryString = `SELECT * FROM droids;`;
+    const queryString = `
+    SELECT droids.* FROM droids
+    LEFT OUTER JOIN purchases ON droid_id = droids.id
+    WHERE droid_id IS NULL;
+    `;
     db.query(queryString)
       .then((data) => {
         const droids = data.rows;
@@ -13,6 +20,18 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
+
+  // GET: droid by manufacturer
+  // RETURN: json object
+  // ACCESS: public
+
+  // GET: droid by model
+  // RETURN: json object
+  // ACCESS: public
+
+  // GET: droid by price
+  // RETURN: json object
+  // ACCESS: public
 
   return router;
 };
