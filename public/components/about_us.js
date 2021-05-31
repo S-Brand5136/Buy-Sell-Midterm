7850fcd0bdd59d16c1bd18ad1f50bf742ec40b2a
Matthew Thompson <supra__charged@hotmail.com>
2021-05-30 18:36:35 -0600

    Initial about us section component created

---
+++ public/components/about_us.js
@@ -0,0 +1,11 @@
+const aboutUs = function() {
+  let aboutUsString = `
+    <section id="about-us">
+      <div>We are the number one ranked droid reclaimers in the galaxy seven years running.</div>
+      <div>All of our droids are guaranteed legally sourced.</div>
+      <div>Galactic scale with firendly service, we offer various interstellar delivery options.</div>
+    </section>
+  `;
+
+  return aboutUsString;
+};
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,19 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/droids/featured"
-  }).done((droids) => {
+  })
+  .then((droids) => {
+    // Add carousel to <main>
     $('#main-content').append(featuredDroids(droids));
-  });;
+  })
+  .then(() => {
+    // Start carousel.
+    const myCarousel = document.querySelector('#featuredCarousel');
+    console.log('myCarousel', myCarousel);
+    const carousel = new bootstrap.Carousel(myCarousel);
+
+    // Add about section below carousel.
+    $('#main-content').append(aboutUs());
+  })
+  .catch(err => console.error(err));
 });
