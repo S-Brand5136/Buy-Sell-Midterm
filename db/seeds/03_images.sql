0b46eccef40336763393a413031de92689c26ddf
Matthew Thompson <supra__charged@hotmail.com>
2021-05-31 15:15:52 -0600

    Add extra C3P0 image to image seed data

--- db/seeds/03_images.sql
+++ db/seeds/03_images.sql
@@ -3,4 +3,5 @@ VALUES
 (1, true, 'images/droid_images/R1_series.webp'),
 (2, true, 'images/droid_images/p2_series.webp'),
 (3, true, 'images/droid_images/bb-8jpg'),
-(4, true, 'images/droid_images/c3po.jpg');
+(4, true, 'images/droid_images/c3po.jpg'),
+(4, true, 'images/droid_images/c3po-2.jpeg');
