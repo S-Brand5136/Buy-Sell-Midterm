3b5da182e2137590265f4bb5814f38aeb35903ed
Matthew Thompson <supra__charged@hotmail.com>
2021-06-01 15:32:22 -0600

    Initial styling for userpage layout

--- public/components/user_page.js
+++ public/components/user_page.js
@@ -1,28 +1,29 @@
 const userPage = function(user) {
   let userString = `
+  <h2 class="user-page-title">${user.name}</h2>
   <div id="user-content">
     <section>
-      <h2>${user.name}</h2>
       <img src="../${user.avatar_url}">
       <ul>
-        <li>${user.email}</li>
-        <li>${user.mobile_phone}</li>
+        <li><span>Email:</span> ${user.email}</li>
+        <li><span>Phone:</span> ${user.mobile_phone}</li>
+        <li><span>Date Joined:</span> ${new Date(user.created_at).toLocaleDateString()}</li>
   `;
 
   if (user.is_admin) {
-    userString += '<li>Administrator</user>'
+    userString += `
+    <li>
+      <span class="badge rounded-pill bg-primary">Administrator</span>
+    </li>`
   }
 
-  userString += `
-  <li>${user.created_at}</li>
-</ul>
-  `;
+  userString += '</ul>';
 
   if (user.is_admin) {
     userString += `
       <div>
         <button class="btn btn-danger type="button onClick="deleteEverything">
-          Delete All Droid Listings!
+          <i class="fas fa-exclamation-triangle"></i>Delete All Droid Listings!
         </button>
       </div>`
   }
