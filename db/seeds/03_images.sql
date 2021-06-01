acaf3366205384ecc03741b5b156fe875079b765
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-01 00:02:00 +0000

    fix spelling mistake in insert

--- db/seeds/03_images.sql
+++ db/seeds/03_images.sql
@@ -2,5 +2,5 @@ INSERT INTO images (droids_id, is_primary, image_url)
 VALUES
 (1, true, 'images/droid_images/R1_series.webp'),
 (2, true, 'images/droid_images/p2_series.webp'),
-(3, true, 'images/droid_images/bb-8jpg'),
+(3, true, 'images/droid_images/bb-8.jpg'),
 (4, true, 'images/droid_images/c3po.jpg');
