d5a2d7540d472b233bb6f670628e556b44203266
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-01 02:57:24 +0000

    add missing droid img

--- db/seeds/03_images.sql
+++ db/seeds/03_images.sql
@@ -4,4 +4,5 @@ VALUES
 (2, true, 'images/droid_images/p2_series.webp'),
 (3, true, 'images/droid_images/bb-8.jpg'),
 (4, true, 'images/droid_images/c3po.jpg'),
-(4, false, 'images/droid_images/c3po-2.jpeg');
+(4, false, 'images/droid_images/c3po-2.jpeg'),
+(5, true, 'images/droid_images/DUM-series_pit_droid.webp');
