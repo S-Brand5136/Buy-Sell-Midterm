f950f81030a519409d2f101cae72f7b85a1a0e67
Matthew Thompson <supra__charged@hotmail.com>
2021-06-02 16:47:19 -0600

    Edit about us content and styling

--- public/components/about_us.js
+++ public/components/about_us.js
@@ -1,9 +1,21 @@
 const aboutUs = function() {
   let aboutUsString = `
     <section id="about-us">
-      <div>We are the number one ranked droid reclaimers in the galaxy seven years running.</div>
-      <div>All of our droids are guaranteed legally sourced.</div>
-      <div>Galactic scale with firendly service, we offer various interstellar delivery options.</div>
+      <div>
+        <h4>The Future is Friendly</h4>
+        <p>At Droid Reclaimers we have been connecting salvagers and buyers across the galaxy for the past 11 years.
+        From our office on Alderan.</p>
+      </div>
+      <div>
+        <h4># 1 Ranked Seller</h4>
+        <p>The galaxy's number one source for reclaimed droids.</p>
+        <p>Consistently ranked as the best droid trading platform by <em>Droid Magazine</em>!</p>
+      </div>
+      <div>
+        <h4>Legal Droids</h4>
+        <p>All of our droids are verified legally sourced so you do not need to worry about any
+        <q>Emperial entanglements</q>.</p>
+      </div>
     </section>
   `;
 
