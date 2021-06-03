aa7ad551b9434535b92badbde08495501c993605
Matthew Thompson <supra__charged@hotmail.com>
2021-06-03 14:16:55 -0600

    Fix nav links in header so they do not refres page

--- public/components/header.js
+++ public/components/header.js
@@ -1,24 +1,21 @@
 function updateHeader(user) {
   return`
-      <div class="container-fluid flex-row justify-content-end">
-      ${user ? `
-      <span>Welcome: <a href="#" class="links" data-destination='/user/${user.id}'>${user.name}</a></span>
-      <a id="logout">Log out</a>`
-       :
-       `<a class="me-3" id='login-btn'>Log in</a>`}
-      </div>
-      <div class="container-fluid flex-row justify-content-between">
-      <h3><a class="links" data-destination="/" href="#">Droid Reclaimers</a></h3>
-      <ul class="nav justify-content-center align-items-center">
-        <li class='nav-item me-5'>
-          <a class="links" data-destination="/droids" href="#">Shop for Droids</a>
-        </li>
-        <li class='nav-item'>
-          ${user ? `<span>Welcome: <a href='/user/${user.id}'>${user.name}</a></span><a id="logout">Log out</a>`
-        : `<a class="" id='login-btn'>Log in</a>`}
-        </li>
-        </ul>
-     </div>`;
+    <div class="container-fluid flex-row">
+    <h3><a href="#" class="links" data-destination='/'>Droid Reclaimers <img src='../images/r2d2-icon.svg'></a></h3>
+    <ul class="nav">
+      <li class='nav-item'>
+        <a class="links" data-destination="/droids" href="#">Shop for Droids</a>
+      </li>
+      <li class='nav-item'>
+        ${user && user.is_admin ? `<button class="btn btn-primary nav-item" data-bs-toggle="modal" data-bs-target="#createDroidModal">Create Listing</button>` : ''}
+      </li>
+      <li class='nav-item'>
+        ${user ? `<span>Welcome: <a href="#" class="links" data-destination='/user/${user.id}'>${user.name}</a></span><a id="logout">Log out</a>`
+      : `<a id='login-btn'>Log in</a>`}
+      </li>
+      </ul>
+   </div>
+  `;
 }
 
 $('body').on('click', '#login-btn', function(event) {
