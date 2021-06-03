ed5e5e4aa0ea30b39f0db1af7d85a2216769f101
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-06-03 00:53:07 +0000

    fix favourite and view button alignment

--- public/components/droid_card.js
+++ public/components/droid_card.js
@@ -14,7 +14,7 @@ const droid_card = (droid, favourite) => {
           <p><strong class='text-primary list-label'>Manufacturer:</strong> ${droid.manufacturer}</p>
           <p><strong class='text-primary list-label'>Model:</strong> ${droid.model}</p>
           <p><strong class='text-primary list-label'>Price:</strong> ${droid.price} credits</p>
-          <div>
+          <div class='d-flex'>
             <span id='favourite-btn' class="${favourite ? "favourite" : "un-favourite"}"><i class="fas fa-heart"></i></span>
             <a href='/droids/${droid.id}' class="btn btn-primary btn-lg">View <i class="fas fa-robot"></i><a>
           </div>
